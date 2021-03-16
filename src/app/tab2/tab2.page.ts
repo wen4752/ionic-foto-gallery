ace8d936786e39b75e1ce91b00cd7991b51eeb7c
mygit <wenaudi74@gmail.com>
2021-03-17 00:38:47 +0800

    commit game

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {ToastController} from '@ionic/angular'
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  angka:Number
+  jawaban:Number
+  pesan:string
+  ngOnInit(){
+    this.jawaban=Math.floor((Math.random()*10)+1)
+    console.log(this.jawaban)
+  }
+  constructor(public toastControllor:ToastController) {}
+
+  tebak(){
+    if(this.angka==this.jawaban){
+      this.pesan='jawaban anda benar'
+    }else{
+      this.pesan='jawaban anda salah'
+    }
+    this.presentToast()
+  }
+
+    async presentToast(){
+      const toast = await this.toastControllor.create({
+        message:this.pesan,
+        duration:1000
+      });
+      toast.present();
+    }
 
 }
